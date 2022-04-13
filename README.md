93b6709195e48a322d644406c4d317c59350251e
eduddz <esteves-dorta@hotmail.com>
2022-04-13 17:24:30 -0300

    Adicionando typescript e implementando a forma de desenvolver

--- README.md
+++ README.md
@@ -15,3 +15,13 @@ Instalando Typescript
     yarn add typescript -D
     |
     yarn add @types/node -D
+
+Para criar o servidor
+
+    yarn add express
+    |
+    yarn add @types/express -D
+
+Para ficar atualizando o servidor
+
+    yarn add ts-node-dev -D
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Deu certo");
+import express, { NextFunction, Request, Response } from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ message: "deu certoooooooo!" });
+})
+
+app.listen(port, () => {
+    console.log("Server running on port " + port + " 🔥🔥🔥🔥🔥")
+})
